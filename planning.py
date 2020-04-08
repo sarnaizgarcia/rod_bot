e6ace9671c88a3216324b256454821776838b424
Silvia <sarnaizgarcia@gmail.com>
2020-04-08 18:29:04 +0200

    Firsta data sketch

---
+++ planning.py
@@ -0,0 +1,39 @@
+import datetime
+
+
+homework_planning = {
+    '2020-04-08': {
+        'Matemáticas': {
+            'title': 'Ficha 1 de Matemáticas',
+            'description': 'Completa los ejercicios',
+            'source': 'https://drive.google.com/open?id=1oo-2a8bV55DmiCmS9GnZ019Agi1bX8WA',
+        },
+        'Lenguaje': {
+            'title': 'Dictado',
+            'description': 'Escribe las palabras del dictado. Escribe en rojo el día de la semana y la fecha. Debajo, escribe en azul 'Dictado' y subráyalo.'
+            'source': None,
+        },
+        'CC Naturales': {
+            'title': 'Ficha 2 de CC Naturales',
+            'description': 'Completa los ejercicios',
+            'source': 'https://drive.google.com/open?id=1t22YqBMPkT50MkcbuMLCfnuNsSFxjPgz'
+        },
+    },
+    '2020-04-09': {
+        'Lectura': {
+            'title': 'Cuento Covid-19',
+            'description': 'Lee el cuento',
+            'source': None,
+        },
+        'Lenguaje': {
+            'title': 'Dictado',
+            'description': 'Escribe las palabras del dictado. Escribe en rojo el día de la semana y la fecha. Debajo, escribe en azul 'Dictado' y subráyalo.'
+            'source': None,
+        },
+        'Matemáticas': {
+            'title': 'Cálculo mental',
+            'description': 'Escribe el resultado de las siguientes sumas: ',
+            'source': None,
+        },
+    },
+}
