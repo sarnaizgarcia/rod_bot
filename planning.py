a8d61040440edd442b9929934e39db21d2a554ee
Silvia <sarnaizgarcia@gmail.com>
2020-04-10 12:17:27 +0200

    Added type key to tasks

--- planning.py
+++ planning.py
@@ -1,39 +1,42 @@
-import datetime
-
-
 homework_planning = {
-    '2020-04-08': {
+    '2020-04-10': {
         'Matemáticas': {
             'title': 'Ficha 1 de Matemáticas',
             'description': 'Completa los ejercicios',
             'source': 'https://drive.google.com/open?id=1oo-2a8bV55DmiCmS9GnZ019Agi1bX8WA',
+            'type': 'worksheet',
         },
         'Lenguaje': {
             'title': 'Dictado',
-            'description': 'Escribe las palabras del dictado. Escribe en rojo el día de la semana y la fecha. Debajo, escribe en azul 'Dictado' y subráyalo.'
-            'source': None,
+            'description': 'Escribe las palabras del dictado. Escribe en rojo el día de la semana y la fecha. Debajo, escribe en azul \'Dictado\' y subráyalo.',
+            'source': 'https://drive.google.com/open?id=1HqCzs8p9XeldsHdpbAcPEQNVl0glxqoj',
+            'type': 'dictation',
         },
         'CC Naturales': {
             'title': 'Ficha 2 de CC Naturales',
             'description': 'Completa los ejercicios',
-            'source': 'https://drive.google.com/open?id=1t22YqBMPkT50MkcbuMLCfnuNsSFxjPgz'
+            'source': 'https://drive.google.com/open?id=1t22YqBMPkT50MkcbuMLCfnuNsSFxjPgz',
+            'type': 'worksheet',
         },
     },
-    '2020-04-09': {
+    '2020-04-11': {
         'Lectura': {
             'title': 'Cuento Covid-19',
             'description': 'Lee el cuento',
-            'source': None,
+            'source': 'https://www.yumpu.com/es/document/read/63156813/coronavirus-no-es-un-principe-ni-una-princesa',
+            'type': 'link',
         },
         'Lenguaje': {
             'title': 'Dictado',
-            'description': 'Escribe las palabras del dictado. Escribe en rojo el día de la semana y la fecha. Debajo, escribe en azul 'Dictado' y subráyalo.'
-            'source': None,
+            'description': 'Escribe las palabras del dictado. Escribe en rojo el día de la semana y la fecha. Debajo, escribe en azul \'Dictado\' y subráyalo.',
+            'source': 'https://drive.google.com/open?id=1HqCzs8p9XeldsHdpbAcPEQNVl0glxqoj',
+            'type': 'dictation',
         },
         'Matemáticas': {
             'title': 'Cálculo mental',
-            'description': 'Escribe el resultado de las siguientes sumas: ',
-            'source': None,
+            'description': 'Escribe el resultado de las siguientes operaciones: ',
+            'source': 'https://arbolabc.com/',
+            'type': 'mental arithmetic',
         },
     },
 }
