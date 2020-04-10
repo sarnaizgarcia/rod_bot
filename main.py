cc38ccfe6dd8cb2e43c3126b51f124f1f3fdc599
Silvia <sarnaizgarcia@gmail.com>
2020-04-10 13:54:26 +0200

    Show tasks function improved

--- main.py
+++ main.py
@@ -18,16 +18,17 @@ def send_welcome(message):
         resize_keyboard=True, one_time_keyboard=False)
     start_markup.row('/tareas', '/ayuda', '/ocultar')
     bot.send_message(message.from_user.id,
-                     '''¡¡Hola!! Dale a /ayuda para ver cómo funciona el bot.\
+                     '''¡¡Hola!! Elije /ayuda para ver cómo funciona el bot.\
                     Puedes ocultar las opciones con /ocultar.\
-                    \nMuestra de nuevo las opciones escribiendo /start.''',
+                    \nMuestra de nuevo las opciones escribiendo /start.\
+                    \nSi quieres ver los deberes de hoy elige /tareas''',
                      reply_markup=start_markup)
 
 
 @bot.message_handler(commands=['ocultar'])
 def hide_command(message):
     hide_markup = telebot.types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, 'ocultamos teclado',
+    bot.send_message(message.from_user.id, 'ocultamos teclado',
                      reply_markup=hide_markup)
 
 
@@ -38,12 +39,25 @@ def show_tasks(message):
         message.chat.id, 'Hoy tenemos que trabajar estas asignaturas:')
     for x in daily_tasks:
         bot.send_message(message.chat.id, x)
+    start_markup = telebot.types.ReplyKeyboardMarkup(
+        resize_keyboard=True, one_time_keyboard=False)
+    for subject in daily_tasks:
+        start_markup.row(f'/{subject}')
+    start_markup.row('/ocultar')
     bot.send_message(
-        message.chat.id, '¿Con cuál quieres comenzar?')
+        message.chat.id, '''¿Con cuál quieres comenzar?\
+        \nRecuerda que puedes ocultar las opciones con /ocultar.''',
+        reply_markup=start_markup)
 
 
-@bot.message_handler(commands=subjects)
-pass
+# @bot.message_handler(commands=['subjects'])
+# def choose_task(message):
+#     start_markup = telebot.types.ReplyKeyboardMarkup(
+#         resize_keyboard=True, one_time_keyboard=False)
+#     for _ in subjects:
+#         start_markup.row(_)
+
+
 # function that uploads work sheets
 # function that uploads audio file
 # function that asks for sums or substractions
