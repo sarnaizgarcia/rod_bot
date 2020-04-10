69bd35f9f8859f7dc3a573e7551bc9c191975482
Silvia <sarnaizgarcia@gmail.com>
2020-04-10 13:36:17 +0200

    Show daily tasks message handler

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from datetime import date
+
 import telebot
 from telebot import types
 
+from get_daily_tasks import get_daily_tasks
 from secrets import token
 
 bot = telebot.TeleBot(token)
+
+subjects = ['lectura', 'matemáticas', 'cc naturales', 'cc sociales',
+            'educación física', 'lengua', 'inglés', 'tecnología', 'música', 'plástica']
 
 
 @bot.message_handler(commands=['start'])
@@ -23,10 +29,24 @@ def hide_command(message):
     hide_markup = telebot.types.ReplyKeyboardRemove()
     bot.send_message(message.chat.id, 'ocultamos teclado',
                      reply_markup=hide_markup)
+
+
+@bot.message_handler(commands=['tareas'])
+def show_tasks(message):
+    daily_tasks = get_daily_tasks()
+    bot.send_message(
+        message.chat.id, 'Hoy tenemos que trabajar estas asignaturas:')
+    for x in daily_tasks:
+        bot.send_message(message.chat.id, x)
+    bot.send_message(
+        message.chat.id, '¿Con cuál quieres comenzar?')
+
+
+@bot.message_handler(commands=subjects)
+pass
 # function that uploads work sheets
 # function that uploads audio file
 # function that asks for sums or substractions
 # function that tells the exersices in English
-
-
 bot.polling()
+# https://github.com/irevenko/info-bot/blob/master/bot.py
